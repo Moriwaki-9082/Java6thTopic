11d8bb035855a82d9de594ba7a623600a3c0ce3e
Moriwaki <mttfmh@icloud.com>
2023-11-15 15:43:13 +0900

    名前、年齢、平均年齢を表示する内容へ変更

---
+++ src/main/java/com/example/Java6thTopic/Average.java
@@ -0,0 +1,22 @@
+package com.example.Java6thTopic;
+
+import java.util.List;
+
+public class Average {
+
+    private final List<Parson> members;
+    private final com.example.Java6thTopic.AverageText averageText;
+
+    public Average(List<Parson> members, com.example.Java6thTopic.AverageText averageText) {
+        this.members = members;
+        this.averageText = averageText;
+    }
+
+    public List<Parson> getMembers() {
+        return members;
+    }
+
+    public com.example.Java6thTopic.AverageText getAverageText() {
+        return averageText;
+    }
+}
---
+++ src/main/java/com/example/Java6thTopic/Parson.java
@@ -0,0 +1,19 @@
+package com.example.Java6thTopic;
+
+public class Parson {
+    private final String name;
+    private final int age;
+
+    public Parson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
