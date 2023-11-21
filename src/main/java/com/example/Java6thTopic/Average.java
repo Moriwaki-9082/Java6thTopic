c5bc0025f1aeb94581a3614d6fda94e38f95ecb2
Moriwaki <mttfmh@icloud.com>
2023-11-21 13:24:56 +0900

    コメント対応修正

--- src/main/java/com/example/Java6thTopic/Average.java
+++ src/main/java/com/example/Java6thTopic/Average.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Average {
 
     private final List<Parson> members;
-    private final com.example.Java6thTopic.AverageText averageText;
+    private final AverageText averageText;
 
     public Average(List<Parson> members, com.example.Java6thTopic.AverageText averageText) {
         this.members = members;
--- src/main/java/com/example/Java6thTopic/Java6thTopicController.java
+++ src/main/java/com/example/Java6thTopic/Java6thTopicController.java
@@ -6,16 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Java6thTopicController {
-    @GetMapping("/averageAge")
-    public Average AverageAge() {
-        int ManAge1 = 28;
-        int ManAge2 = 32;
-        int WomanAge1 = 21;
+    @GetMapping("/average-age")
+    public Average averageAge() {
+        int manAge1 = 28;
+        int manAge2 = 32;
+        int womanAge1 = 21;
         return new Average(List.of(
-                new Parson("Bob", ManAge1),
-                new Parson("Jon", ManAge2),
-                new Parson("KAte", WomanAge1)
+                new Parson("Bob", manAge1),
+                new Parson("Jon", manAge2),
+                new Parson("KAte", womanAge1)
         ),
-                new AverageText("平均年齢は" + (ManAge1 + ManAge2 + WomanAge1) / 3 + "歳です"));
+                new AverageText("平均年齢は" + (manAge1 + manAge2 + womanAge1) / 3 + "歳です"));
     }
 }
